7e507cf1303118e52503e5b76691a6ee53f0d7b2
Jedaiah Tantanella <jedanne143@gmail.com>
2024-10-09 19:23:40 -0400

    changed styling on About page

--- frontend/Salon/src/pages/About.jsx
+++ frontend/Salon/src/pages/About.jsx
@@ -21,16 +21,18 @@ function About() {
       <h2 className='mainHeading'>Visit Us</h2>
       <div className="rowInitially">
         <div className='businessLeftCol'>
-          <iframe
-            className='googleMap'
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d965.0869285636699!2d121.0275595849788!3d14.636192890559546!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3397b75118d27c9b%3A0x3cc5206358c8dd87!2sMakeover%20Beauty%20Lounge!5e0!3m2!1sen!2sus!4v1728413393068!5m2!1sen!2sus"
-            width="400"
-            height="300"
-            style={{ border: 0 }}
-            allowFullScreen
-            loading="lazy"
-            referrerPolicy="no-referrer-when-downgrade"
-          />
+          <div className='iframeContainer'>
+            <iframe
+              className='googleMap'
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d965.0869285636699!2d121.0275595849788!3d14.636192890559546!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3397b75118d27c9b%3A0x3cc5206358c8dd87!2sMakeover%20Beauty%20Lounge!5e0!3m2!1sen!2sus!4v1728413393068!5m2!1sen!2sus"
+              width="400"
+              height="300"
+              style={{ border: 0 }}
+              allowFullScreen
+              loading="lazy"
+              referrerPolicy="no-referrer-when-downgrade"
+            />
+          </div>
         </div>
         <div className='businessCol'>
           <h2 className='typeHeading'>Business Hours</h2>
